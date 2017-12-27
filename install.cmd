0fddf5a386e0d4438fdf0b972267af2c5701f359
martiniturbide <martin@os2world.com>
2017-12-27 09:40:19 -0500

    Beat203-r3

--- install.cmd
+++ install.cmd
@@ -6,25 +6,26 @@
  * > Renewal
  * Revision 2 (18.Jan.2001)
  * > Fix problem.
+ * Revision 3 (13.Nov.2017)
+ * > Root directory support, folder icons.
  *
  *
  */
 rc = RxFuncAdd( "SysLoadFuncs", "RexxUtil", "SysLoadFuncs")
 
 DIR = directory()
+IF Right(DIR,1)<>'\' THEN DIR=DIR'\'
 
-APL_FOLDER_NAME = "Beat/2"
-APL_FOLDER_ID   = "<BEAT2_FOLDER>"
+APL_FOLDER_NAME  = "Beat/2"
+APL_FOLDER_ID    = "<BEAT2_FOLDER>"
+APL_FOLDER_ICONS = "ICONFILE="DIR"Beat_C.ICO;ICONNFILE=1,"DIR"Beat_O.ICO"
 
 APL_EXE_NAME = "Beat/2"
-APL_EXE_FILE = DIR"\BEAT.EXE"
+APL_EXE_FILE = DIR"BEAT.EXE"
 APL_EXE_ID   = "<BEAT2>"
 
-README_E_FILE = DIR"\readme_e.htm"
+README_E_FILE = DIR"readme_e.htm"
 README_E_NAME = "Readme English"
-
-README_J_FILE = DIR"\readme_j.htm"
-README_J_NAME = "Readme Japanese"
 
 
 say ""
@@ -38,7 +39,7 @@ if SysCreateObject( 'WPFolder', 'Pretty Pop Software', '<WP_DESKTOP>', "OBJECTID
 end
 
 /* 2. Application Folder */
-if SysCreateObject( 'WPFolder', APL_FOLDER_NAME, '<PRETTY_POP_SOFTWARE>', "OBJECTID="APL_FOLDER_ID, "u" ) then do
+if SysCreateObject( 'WPFolder', APL_FOLDER_NAME, '<PRETTY_POP_SOFTWARE>', "OBJECTID="APL_FOLDER_ID";"APL_FOLDER_ICONS,"u" ) then do
     say " Create Folder : "APL_FOLDER_NAME
 end
 
@@ -52,12 +53,6 @@ if SysCreateShadow("<WP_START>", APL_FOLDER_ID) then do
     say " Create Shadow : Startup Folder"
 end
 
-/*
-if SysCreateObject( 'WPUrl', README_J_NAME, APL_FOLDER_ID, 'URL=file:///'README_J_FILE, "u" ) then do
-    say " Create Object : "README_J_NAME
-end
-*/
-
 if SysCreateObject( 'WPUrl', README_E_NAME, APL_FOLDER_ID, 'URL=file:///'README_E_FILE, "u" ) then do
     say " Create Object : "README_E_NAME
 end
@@ -68,4 +63,3 @@ say " Please restart OS/2 or start Beat/2 in the startup folder."
 say ""
 say " Beat/2 のインストールは無事に終了しました。"
 say " OS/2をリスタートするか始動フォルダのBeat/2を起動してください。"
-
